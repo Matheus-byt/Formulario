78ae82cf281a944ddd10b1c50db7db2d410faa7d
LUAN CHAVES DE SOUSA <76760576+DevMboo@users.noreply.github.com>
2022-08-04 18:06:53 -0300

    Valid forms

---
+++ script.js
@@ -0,0 +1,13 @@
+const formValid = () => {
+    addEventListener('click', function(event){
+        console.log(event.target)
+
+        if(document.getElementById('timeone').value == ''){
+            event.preventDefault()
+            this.alert('Preencha todos os campos')
+        }
+
+    })
+}
+
+formValid()
