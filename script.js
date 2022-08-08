ebcfe9c1e14450ae386c133e6d371e5d0ae64e63
unknown <Administrador@ZL-DESENV4251.tmkt.servicos.mkt>
2022-08-08 09:40:25 -0300

    Add SweetAlert2

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ const formValid = () => {
 
         if(data.value, timeone.value, timeend.value, assinatura_tecnico.value == ''){
             event.preventDefault()
-            console.log('todos os campos vázios')
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Você esqueceu de preencher um campo!',
+                iconColor: '#002d68',
+                confirmButtonColor: '#002d68'
+                //footer: '<a href="">Why do I have this issue?</a>'
+              })
         }
 
     })
